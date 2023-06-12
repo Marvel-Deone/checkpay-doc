28a75b3309c0761378ff9c3e0d06ef24f419f0ab
Folagbade Olowofela <89777325+Marvel-Deone@users.noreply.github.com>
2023-06-12 09:37:22 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DocsComponent } from './docs/docs.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'docs-page', component: DocsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MenuComponent } from './public/menu/menu.component';
+import { FooterComponent } from './public/footer/footer.component';
+import { DocsComponent } from './docs/docs.component';
+import { HeaderMenuComponent } from './public/header-menu/header-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    MenuComponent,
+    FooterComponent,
+    DocsComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule,
